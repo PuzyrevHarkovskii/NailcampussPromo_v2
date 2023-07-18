ffe2f0688f39650c5f0ae8699cad5741257a85d1
Владислав <90913741+PuzyrevHarkovskii@users.noreply.github.com>
2023-07-18 23:08:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -138,3 +138,70 @@ function nope() {
 
 $('#like-button').on('click', like);
 $('#nope-button').on('click', nope);
+
+
+
+// Палец который указывает что можно свайпать
+
+
+const svgElement = document.getElementById("custom-swipe-horizontal");
+let touchStartX = 0;
+let touchEndX = 0;
+
+function hideObject() {
+  svgElement.classList.add("hide");
+  setTimeout(() => {
+    svgElement.style.display = "none";
+  }, 300);
+}
+
+function handleTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+  touchEndX = event.touches[0].clientX;
+}
+
+function handleTouchEnd() {
+  const swipeDistance = touchEndX - touchStartX;
+  if (Math.abs(swipeDistance) > 1) { // Пороговое значение для определения свайпа
+    hideObject();
+  }
+}
+
+svgElement.addEventListener("click", hideObject);
+svgElement.addEventListener("touchstart", handleTouchStart);
+svgElement.addEventListener("touchmove", handleTouchMove);
+svgElement.addEventListener("touchend", handleTouchEnd);
+
+setTimeout(hideObject, 4000);
+
+
+
+// Закрытие модального окна по щелчку в любой области
+
+ var modals = document.getElementsByClassName("overlay");
+
+  // Добавляем обработчик события клика для каждого модального окна
+  Array.from(modals).forEach(function(modal) {
+    modal.addEventListener("click", function(event) {
+      // Проверяем, является ли цель клика модальным окном
+      if (event.target === modal) {
+        // Закрываем модальное окно
+        modal.style.display = "none";
+      }
+    });
+  });
+
+
+// Получаем ссылки или кнопки, открывающие модальные окна
+var modalLinks = document.querySelectorAll(".button[href^='#popup']");
+
+// Добавляем обработчик события клика для каждой ссылки или кнопки
+Array.from(modalLinks).forEach(function(link) {
+  link.addEventListener("click", function(event) {
+    var target = document.querySelector(link.getAttribute("href"));
+    target.style.display = "block";
+  });
+});
